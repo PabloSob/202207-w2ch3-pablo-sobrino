77ab9c2359028733bd68d92eade436bb3d7dad42
Pablo S <sobrinopablo@gmail.com>
2022-07-13 19:33:33 +0200

    pasapalabra added

---
+++ Precurso/pasapalabra/Pasapalabra.js
@@ -0,0 +1,279 @@
+const letraA = [
+	{letter: "a", answer: "abducir", status: 0, question: ("CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien")},
+	{letter: "a", answer: "anticuario", status: 0, question: ("CON LA A. Persona que colecciona o negocia con antigüedades")},
+	{letter: "a", answer: "ala", status: 0, question: ("CON LA A. Cada uno de los órganos o apéndices pares que utilizan algunos animales para volar.")},
+]
+
+const letraB = [
+	{letter: "b", answer: "bingo", status: 0, question: ("CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso")},
+	{letter: "b", answer: "biberon", status: 0, question: ("CON LA B. Utensilio para la lactancia artificial que consiste en una botella pequeña con una tetina de goma")},
+	{letter: "b", answer: "bocadillo", status: 0, question: ("CON LA B. En las viñetas de cómics, espacio rodeado de una línea donde está escrito lo que dice o piensa un personaje")},
+]
+
+const letraC = [
+	{letter: "c", answer: "churumbel", status: 0, question: ("CON LA C. Niño, crío, bebé")},
+	{letter: "c", answer: "capo", status: 0, question: ("CON LA C. Jefe de la mafia, especialmente de narcotraficantes")},
+	{letter: "c", answer: "cinturon", status: 0, question: ("CON LA C. Banda, por ejemplo de cuero, con que se sujetan los vestidos a la cintura")},
+]
+
+const letraD = [
+	{letter: "d", answer: "diarrea", status: 0, question: ("CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida")},
+	{letter: "d", answer: "desodorante", status: 0, question: ("CON LA D. Producto que se utiliza para suprimir el olor corporal o de algún recinto")},
+	{letter: "d", answer: "dialogo", status: 0, question: ("CON LA D. Conversación entre dos o más personas, que alternativamente manifiestan sus ideas o afectos")},
+]
+
+const letraE = [
+	{letter: "e", answer: "ectoplasma", status: 0, question: ("CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación")},
+	{letter: "e", answer: "efimero", status: 0, question: ("CON LA E. Pasajero, de corta duración")},
+	{letter: "e", answer: "espejo", status: 0, question: ("CON LA E. Superficie brillante que refleja las imágenes")},
+]
+
+const letraF = [
+	{letter: "f", answer: "facil", status: 0, question: ("CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad")},
+	{letter: "f", answer: "faja", status: 0, question: ("CON LA F. Prenda interior elástica que ciñe la cintura, o la cintura y las caderas")},
+	{letter: "f", answer: "fuellar", status: 0, question: ("CON LA F. Talco de colores con que se adornan la velas rizadas")},
+]
+
+const letraG = [
+	{letter: "g", answer: "galaxia", status: 0, question: ("CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas")},
+	{letter: "g", answer: "gominola", status: 0, question: ("CON LA G. Golosina blanca masticable, generalmente recubierta de azúcar ")},
+	{letter: "g", answer: "garganta", status: 0, question: ("CON LA G. Espacio interno comprendido entre el velo del paladar y la entrada del esófago y de la laringe")},
+]
+
+const letraH = [
+	{letter: "h", answer: "harakiri", status: 0, question: ("CON LA H. Suicidio ritual japonés por desentrañamiento")},
+	{letter: "h", answer: "historia", status: 0, question: ("CON LA H. Conjunto de todos los hechos ocurridos en tiempos pasados ")},
+	{letter: "h", answer: "hematoma", status: 0, question: ("CON LA H. Mancha amoratada que se hace en el cuerpo a consecuencia de un golpe")},
+]
+
+const letraI = [
+	{letter: "i", answer: "iglesia", status: 0, question: ("CON LA I. Templo cristiano")},
+	{letter: "i", answer: "ilves", status: 0, question: ("CON LA I. Apellido del político que fue presidente de Estonia entre los años 2006 y 2016")},
+	{letter: "i", answer: "imanishi", status: 0, question: ("CON LA I. Apellido del montañero que, junto al sherpa Gyalzen Norbu, alcanzó la cumbre del Manaslu en 1956")},
+]
+
+const letraJ = [
+	{letter: "j", answer: "jabali", status: 0, question: ("CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba")},
+	{letter: "j", answer: "judion", status: 0, question: ("CON LA J. Variedad de judía de vainas anchas y semilla grande")},
+	{letter: "j", answer: "jugo", status: 0, question: ("CON LA J. Líquido que se extrae de determinadas frutas o verduras")},
+]
+
+const letraK = [
+	{letter: "k", answer: "kamikaze", status: 0, question: ("CON LA K. Persona que se juega la vida realizando una acción temeraria")},
+	{letter: "k", answer: "ku klux klan", status: 0, question: ("CON LA K. Grupo extremista norteamericano conocido por sus tintes xenofóbicos")},
+	{letter: "k", answer: "kaiser", status: 0, question: ("CON LA K. Emperador de Alemania")},
+]
+
+const letraL = [
+	{letter: "l", answer: "licantropo", status: 0, question: ("CON LA L. Hombre lobo")},
+	{letter: "l", answer: "linaje", status: 0, question: ("CON LA L. Ascendencia o descendencia de una familia, especialmente noble")},
+	{letter: "l", answer: "litro", status: 0, question: ("CON LA L. Unidad e volumen equiparable al decímetro cúbico")},
+]
+
+const letraM = [
+	{letter: "m", answer: "misantropo", status: 0, question: ("CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas")},
+	{letter: "m", answer: "madriguera", status: 0, question: ("CON LA M. Cueva en la que habitan ciertos animales, especialmente los conejos")},
+	{letter: "m", answer: "menos", status: 0, question: ("CON LA M. Signo de sustracción o resta")},
+]
+
+const letraN = [
+	{letter: "n", answer: "necedad", status: 0, question: ("CON LA N. Demostración de poca inteligencia")},
+	{letter: "n", answer: "navato", status: 0, question: ("CON LA N. Espinazo de los vertebrados")},
+	{letter: "n", answer: "notificar", status: 0, question: ("CON LA N. Comunicar formalmente a su destinatario una resolución administrativa o judicial")},
+]
+
+const letraÑ = [
+	{letter: "ñ", answer: "señal", status:0, question: ("CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo.")},
+	{letter: "ñ", answer: "acuñar", status: 0, question: ("CON LA Ñ. Hacer o fabricar moneda ")},
+	{letter: "ñ", answer: "legaña", status: 0, question: ("CON LA Ñ. Líquido segregado por la mucosa de los párpados y que se cuaja en los bordes o en los ángulos de los ojos")},
+]
+
+const letraO = [
+	{letter: "o", answer: "orco", status: 0, question: ("CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien")},
+	{letter: "o", answer: "onirico", status: 0, question: ("CON LA O. Perteneciente o relativo a los sueños")},
+	{letter: "o", answer: "orbe", status: 0, question: ("CON LA O. Mundo, conjunto de todo lo existente")},
+]
+
+const letraP = [
+	{letter: "p", answer: "protoss", status: 0, question: ("CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft")},
+	{letter: "p", answer: "piolet", status: 0, question: ("CON LA P. Instrumento con forma de pico que utilizan los alpinistas para asegurar sus movimientos sobre la nieve o el hielo")},
+	{letter: "p", answer: "podolia", status: 0, question: ("CON LA P. Región de Ucrania que se extiende entre los ríos Dniéster y Bug Meridional")},
+]
+
+const letraQ = [
+	{letter: "q", answer: "queso", status: 0, question: ("CON LA Q. Producto obtenido por la maduración de la cuajada de la leche")},
+	{letter: "q", answer: "equilatero", status: 0, question: ("CON LA Q. Triángulo que tiene todos sus lados iguales ")},
+	{letter: "q", answer: "acequia", status: 0, question: ("CON LA Q. Zanja o canal por donde se conducen las aguas para regar y para otros fines")},
+]
+
+const letraR = [
+	{letter: "r", answer: "raton", status: 0, question: ("CON LA R. Roedor")},
+	{letter: "r", answer: "risaralda", status: 0, question: ("CON LA R. Departamento de Colombia con capital en la ciudad de Pereira")},
+	{letter: "r", answer: "rezar", status: 0, question: ("CON LA R. Dirigir a Dios y a los santos oraciones de contenido religioso")},
+]
+
+const letraS = [
+	{letter: "s", answer: "stackoverflow", status: 0, question: ("CON LA S. Comunidad salvadora de todo desarrollador informático")},
+	{letter: "s", answer: "sentar", status: 0, question: ("CON LA S. Poner o colocar a alguien en una silla o banco, de manera que quede apoyado y descansando sobre las nalgas")},
+	{letter: "s", answer: "sordo", status: 0, question: ("CON LA S. Que padece una pérdida auditiva en mayor o menor grado")},
+]
+
+const letraT = [
+	{letter: "t", answer: "terminator", status: 0, question: ("CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984")},
+	{letter: "t", answer: "tipi", status: 0, question: ("CON LA T.  Tienda de piel de forma cónica que utilizaban como vivienda los indios de las praderas de América del Norte")},
+	{letter: "t", answer: "trefilar", status: 0, question: ("CON LA T. Reducir un metal a alambre o hilo pasándolo por una hilera")},
+]
+
+const letraU = [
+	{letter: "u", answer: "unamuno", status: 0, question: ("CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914")},
+	{letter: "u", answer: "unilateral", status: 0, question: ("CON LA U. Que se refiere o se circunscribe solamente a una parte o a un aspecto de algo")},
+	{letter: "u", answer: "urbanidad", status: 0, question: ("CON LA U. Comportamiento en el trato social con el que se muestra amabilidad y educación")},
+]
+
+const letraV = [
+	{letter: "v", answer: "vikingos", status: 0, question: ("CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa")},
+	{letter: "v", answer: "vilano", status: 0, question: ("CON LA V. Flor del cardo")},
+	{letter: "v", answer: "velamen", status: 0, question: ("CON LA V. Conjunto de las velas de un barco")},
+]
+
+const letraW = [
+	{letter: "w", answer: "sandwich", status: 0, question: ("CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso")},
+	{letter: "w", answer: "waterpolo", status: 0, question: ("CON LA W. Deporte que se practica en una piscina, entre dos equipos y con una pelota")},
+	{letter: "w", answer: "walkman", status: 0, question: ("CON LA W. Aparato portátil reproductor de casettes")},
+]
+
+const letraX = [
+	{letter: "x", answer: "botox", status: 0, question: ("CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética")},
+	{letter: "x", answer: "dux", status: 0, question: ("CON LA X. En las repúblicas de Venecia o Génova, príncipe o magistrado supremo")},
+	{letter: "x", answer: "oxitocina", status: 0, question: ("CON LA X. Hormona que estimula la contracción el útero durante el parto y la secreción de leche en la glándula mamaria")},
+]
+
+const letraY = [
+	{letter: "y", answer: "peyote", status: 0, question: ("CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal por indígenas americanos")},
+	{letter: "y", answer: "ensayar", status: 0, question: ("CON LA Y. Preparar el montaje y ejecución de un espectáculo antes de ofrecerlo al público")},
+	{letter: "y", answer: "plebeyo", status: 0, question: ("CON LA Y. Se dice de la persona que no es noble ni hidalga")},
+]
+
+const letraZ = [
+	{letter: "z", answer: "zen", status: 0, question: ("CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional")},
+	{letter: "z", answer: "zumbado", status: 0, question: ("CON LA Z. Participio del verbo zumbar")},
+	{letter: "z", answer: "zapador", status: 0, question: ("CON LA Z. Soldado de un cuerpo destinado a los trabajos de excavación")},
+]
+
+
+let itemA = letraA[Math.floor(Math.random()*letraA.length)]
+let itemB = letraB[Math.floor(Math.random()*letraB.length)]
+let itemC = letraC[Math.floor(Math.random()*letraC.length)]
+let itemD = letraD[Math.floor(Math.random()*letraD.length)]
+let itemE = letraE[Math.floor(Math.random()*letraE.length)]
+let itemF = letraF[Math.floor(Math.random()*letraF.length)]
+let itemG = letraG[Math.floor(Math.random()*letraG.length)]
+let itemH = letraH[Math.floor(Math.random()*letraH.length)]
+let itemI = letraI[Math.floor(Math.random()*letraI.length)]
+let itemJ = letraJ[Math.floor(Math.random()*letraJ.length)]
+let itemK = letraK[Math.floor(Math.random()*letraK.length)]
+let itemL = letraL[Math.floor(Math.random()*letraL.length)]
+let itemM = letraM[Math.floor(Math.random()*letraM.length)]
+let itemN = letraN[Math.floor(Math.random()*letraN.length)]
+let itemÑ = letraÑ[Math.floor(Math.random()*letraÑ.length)]
+let itemO = letraA[Math.floor(Math.random()*letraO.length)]
+let itemP = letraP[Math.floor(Math.random()*letraP.length)]
+let itemQ = letraQ[Math.floor(Math.random()*letraQ.length)]
+let itemR = letraR[Math.floor(Math.random()*letraR.length)]
+let itemS = letraS[Math.floor(Math.random()*letraS.length)]
+let itemT = letraT[Math.floor(Math.random()*letraT.length)]
+let itemU = letraU[Math.floor(Math.random()*letraU.length)]
+let itemV = letraV[Math.floor(Math.random()*letraV.length)]
+let itemW = letraW[Math.floor(Math.random()*letraW.length)]
+let itemX = letraX[Math.floor(Math.random()*letraX.length)]
+let itemY = letraY[Math.floor(Math.random()*letraY.length)]
+let itemZ = letraZ[Math.floor(Math.random()*letraZ.length)]
+
+
+
+
+let rosco = [itemA, itemB, itemC, itemD, itemE, itemF, itemG, itemH, itemJ, itemK, itemL, itemM, itemN, itemÑ, itemO, itemP, itemQ, itemR, itemS, itemT, itemU, itemV, itemW, itemX, itemY, itemZ]
+
+
+let scores = [
+	{name: 'Leonardo', right: 25, wrong: 2},
+	{name: 'Donatello', right: 20, wrong: 7},
+	{name: 'Michelangelo', right: 21, wrong: 6},
+	{name: 'Raphael', right: 23, wrong: 4},
+];
+
+
+
+const askName = () => {
+
+    const newChallenger = prompt ("whats your name?");
+    
+    if (!newChallenger) {
+	return askName();
+    }
+	alert(`Welcome to Pasapalabra! Dear ${newChallenger}`);
+    return newChallenger;
+    };
+	
+	
+	
+const finalRanking = () => {
+		
+	scores.sort(function(a,b){ 
+	return b.right - a.right
+		   
+		})
+	console.table(scores);
+	};
+
+
+const pasaPalabra = () => {
+	let right = 0;
+	let wrong = 0;
+	let count = 0;
+	let newChallenger = askName()
+	
+	do {
+		for(let i=0; i<rosco.length; i++){
+
+			if(rosco[i].status === 0){
+
+			let userAnswer = prompt(rosco[i].question);
+			if (userAnswer != null)
+			userAnswer = userAnswer.toLowerCase()
+
+			if(userAnswer === rosco[i].answer){
+				alert("Correcto!");
+				rosco[i].status = 1;
+				right++;
+				count++;
+			}
+			else if(userAnswer === 'pasapalabra' || userAnswer === '' || userAnswer === null){
+				alert('Pasamos palabra');
+			}
+			else if(userAnswer === 'end'){
+				alert ("Has acertado " + right + " palabras y has cometido " + wrong + " errores.");
+				return
+			}
+			else {
+				alert(`WRONG WORD! The right answer was: ${rosco[i].answer}`);
+				rosco[i].status = 2;
+				wrong++;
+				count++;
+			}
+		}}
+	} while (count < rosco.length);
+	alert("Has acertado " + right + " palabras y has cometido " + wrong + " errores.")
+    scores.push({name: newChallenger, right: right, wrong: wrong})
+	finalRanking();
+	}
+
+pasaPalabra()
+
+
+
+
+
+
+	
+	
